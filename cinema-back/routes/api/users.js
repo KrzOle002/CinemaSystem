4c4fdc74e62d111c212a52a3114a321801e084d4
Xkriso <thekriso@wp.pl>
2023-10-15 00:14:03 +0200

    Added middleware

--- cinema-back/routes/api/users.js
+++ cinema-back/routes/api/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator')
 const gravatar = require('gravatar')
 const bcrypt = require('bcryptjs')
+const jwt = require("jsonwebtoken")
+const config = require('config')
 const User = require('../../models/User')
 // @route   GET api/users
 // @desc    Test route
@@ -46,7 +48,16 @@ router.post("/",
 
             await user.save();
 
-            res.send('User registered')
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
+
+            jwt.sign(payload, config.get('jwtToken'), { expiresIn: 360000 }, (err, token) => {
+                if (err) throw err;
+                res.json({ token });
+            })
         }
         catch (err) {
             console.error(err.message);
