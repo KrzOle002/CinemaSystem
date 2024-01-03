e265b03707f5d40d5bef302d1dd1c3a1063df9dd
Krzysztof Olejniczak <thekriso@wp.pl>
2024-01-03 14:56:02 +0100

    Better view

--- cinema-front/src/pages/AdminPanel/AdminPanel.tsx
+++ cinema-front/src/pages/AdminPanel/AdminPanel.tsx
@@ -1,12 +1,40 @@
 import styled from 'styled-components'
 import MoviePanel from './MoviePanel/MoviePanel'
 import SchedulePanel from './SchedulePanel/SchedulePanel'
+import Accordion from '@mui/material/Accordion'
+import AccordionDetails from '@mui/material/AccordionDetails'
+import AccordionSummary from '@mui/material/AccordionSummary'
+import Typography from '@mui/material/Typography'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import { useState } from 'react'
 const AdminPanel = () => {
+	const [expanded, setExpanded] = useState<string | false>(false)
+
+	const handleChange = (panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+		setExpanded(isExpanded ? panel : false)
+	}
+
 	return (
 		<Wrapper>
 			<Container>
-				<MoviePanel />
-				<SchedulePanel />
+				<StyledAccordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+					<AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls='panel1bh-content' id='panel1bh-header'>
+						<Typography sx={{ width: '100%', textAlign: 'center' }}>Zarządzanie filmami</Typography>
+					</AccordionSummary>
+					<AccordionDetails>
+						<MoviePanel />
+					</AccordionDetails>
+				</StyledAccordion>
+				<StyledAccordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+					<AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls='panel2bh-content' id='panel2bh-header'>
+						<Typography sx={{ width: '100%', textAlign: 'center' }}>Zarządzanie repertuarem</Typography>
+					</AccordionSummary>
+					<AccordionDetails>
+						<Typography>
+							<SchedulePanel />
+						</Typography>
+					</AccordionDetails>
+				</StyledAccordion>
 			</Container>
 		</Wrapper>
 	)
@@ -20,15 +48,12 @@ const Wrapper = styled.div`
 	height: 100%;
 `
 const Container = styled.div`
-	@media screen and (max-width: 640px) {
-		width: 80%;
-	}
 	height: 100%;
-	display: flex;
-	flex-direction: row;
-	gap: 20px;
 	align-items: center;
 	justify-content: center;
-	width: 80%;
+
 	margin: 0 auto;
+`
+const StyledAccordion = styled(Accordion)`
+	background-color: '#ff963f';
 `
